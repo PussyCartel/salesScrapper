ee7ec10d8493d90fe0e79f99042296015e3587e9
PussyCartel <iljinviteok@gmail.com>
2021-12-14 10:07:33 +0300

    delivery spider init

--- salesScrapper/spiders/deliverySpider.py
+++ salesScrapper/spiders/deliverySpider.py
@@ -8,9 +8,13 @@ class DeliveryScrapper(scrapy.Spider):
         urls = [
             'https://www.delivery-club.ru/moscow/promo'
         ]
-
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response, **kwargs):
-        pass
+        pay_attention_block = response.css('div.special-promotion-card__wrapper')
+        sale_name = pay_attention_block.css('div.special-promotion-card__text::text').getall()
+        company_name = pay_attention_block.css('div.special-promotion-card__name::text').getall()
+        for i in range(4):
+            self.log(sale_name[i])
+            self.log(company_name[i])
