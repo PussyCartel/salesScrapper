0de2b91110b78317011795d5e5c1aa7a63b3694f
PussyCartel <iljinviteok@gmail.com>
2021-12-18 08:35:00 +0300

    db fix

--- salesScrapper/pipelines.py
+++ salesScrapper/pipelines.py
@@ -5,9 +5,33 @@
 
 
 # useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+import psycopg2
 
 
-class SalesscrapperPipeline:
+class PostgreSQKPIPELINE(object):
+
+    def open_spider(self, spider):
+        hostname = '178.216.96.119'
+        username = 'postgres'
+        password = 'q4X3YaTh4XeVk5Z'
+        database = 'postgres'
+        self.connection = psycopg2.connect(
+            host=hostname, user=username, password=password,
+            dbname=database)
+        self.cur = self.connection.cursor()  # Define function to disconnect from database
+
+    def close_spider(self, spider):
+        self.cur.close()
+        self.connection.close()  # Define function to process each scraped item and insert it into    PostgreSQL table
+
     def process_item(self, item, spider):
-        return item
+        try:
+            # Execute SQL command on database to insert data in table
+            self.cur.execute(
+                "insert into DeliveryPayAttentionBlock (product_name, product_company) values(%s,%s)",
+                (item['product_name'], item['product_company']))
+            self.connection.commit()
+        except:
+            self.connection.rollback()
+            raise
+            return item
